fd079c9e9d4a42640bf05f33efdab3e73a2df00b
AuroraZiling <2935876049@qq.com>
2022-02-13 21:03:38 +0800

    Updated

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5545dc7cc42ef1671f1fedb60612aec0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
